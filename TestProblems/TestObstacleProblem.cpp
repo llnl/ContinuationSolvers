701f6b0672bcb6579e4d21a5aa0ea8a003862df3
Tucker Hartland <tucker.hartland@gmail.com>
2026-02-17 12:33:44 -0800

    adding in obstacle problem

---
+++ TestProblems/TestObstacleProblem.cpp
@@ -0,0 +1,125 @@
+//                         Obstacle Problem
+//
+//
+// Compile with: make ParObstacleProblem
+//
+// Sample runs: mpirun -np 4 ./ParObstacleProblem
+//
+//
+// Description: This example code demonstrates the use of MFEM to solve the
+//              bound-constrained energy minimization problem
+//
+//                      minimize (||∇u||² + ||u||²) subject to u ≥ ϕ in H¹.
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+#include "../continuationsolvers/problems/Problems.hpp"
+#include "../continuationsolvers/solvers/IPSolver.hpp"
+
+using namespace std;
+using namespace mfem;
+
+double dmanufacturedFun(const Vector &);
+double fRhs(const Vector &);
+
+int main(int argc, char *argv[])
+{
+  // Initialize MPI
+   Mpi::Init();
+   int num_procs = Mpi::WorldSize();
+   int myid = Mpi::WorldRank();
+   Hypre::Init();
+
+   int FEorder = 1; // order of the finite elements
+   int maxIPMiters = 30;
+   int ref_levels = 3;
+   OptionsParser args(argc, argv);
+   args.AddOption(&FEorder, "-o", "--order",\
+	 	  "Order of the finite elements.");
+   args.AddOption(&maxIPMiters, "-IPMiters", "--IPMiters",\
+	 	  "Maximum number of IPM iterations");
+   args.AddOption(&ref_levels, "-r", "--mesh_refinement", \
+		  "Mesh Refinement");
+  
+   args.Parse();
+   if(!args.Good())
+   {
+     args.PrintUsage(cout);
+     return 1;
+   }
+   else
+   {
+     if(Mpi::Root())
+     {  
+       args.PrintOptions(cout);
+     }
+   }
+
+   const char *meshFile = "meshes/inline-quad.mesh";
+   Mesh mesh(meshFile, 1, 1);
+   int dim = mesh.Dimension(); // geometric dimension of the meshed domain
+   {
+      for (int l = 0; l < ref_levels; l++)
+      {
+         mesh.UniformRefinement();
+      }
+   }
+
+   ParMesh pmesh(MPI_COMM_WORLD, mesh);
+
+   FiniteElementCollection *fec = new H1_FECollection(FEorder, dim);
+   ParFiniteElementSpace   *Vh  = new ParFiniteElementSpace(&pmesh, fec);
+
+   ObstacleProblem problem(Vh,Vh,&fRhs);
+  
+   int dimU = problem.GetDimU();
+   Vector x0(dimU); x0 = 100.0;
+   Vector xf(dimU); xf = 0.0;
+
+   InteriorPointSolver optimizer(&problem); 
+   optimizer.SetTol(1.e-8);
+   optimizer.SetMaxIter(maxIPMiters);
+   optimizer.Mult(x0, xf);
+
+   ParGridFunction d_gf(Vh);
+
+   d_gf.SetFromTrueDofs(xf);
+
+
+   FunctionCoefficient dm_fc(dmanufacturedFun); // manufactured solution
+   ParGridFunction dm_gf(Vh);
+   dm_gf.ProjectCoefficient(dm_fc);
+   
+   // 17. Save data in the ParaView format
+   ParaViewDataCollection paraview_dc("Obstacle", &pmesh);
+   paraview_dc.SetPrefixPath("ParaView");
+   paraview_dc.SetLevelsOfDetail(FEorder);
+   paraview_dc.SetDataFormat(VTKFormat::BINARY);
+   paraview_dc.SetHighOrderOutput(true);
+   paraview_dc.SetCycle(0);
+   paraview_dc.SetTime(0.0);
+   paraview_dc.RegisterField("displacement", &d_gf);
+   paraview_dc.RegisterField("displacement (manufactured)", &dm_gf);
+   paraview_dc.Save();
+
+   
+
+
+   delete Vh;
+   delete fec;
+   return 0;
+}
+
+
+double dmanufacturedFun(const Vector &x)
+{
+  return cos(2*M_PI*x(0)) + 0.2 - 2.0*(pow(x(0),3) - 1.5*pow(x(0),2));
+}
+
+double fRhs(const Vector &x)
+{
+  double fx = 0.;
+  fx = 0.2 - 2.0 * (pow(x(0),3)- 1.5*pow(x(0),2.) - 6 * x(0) + 3.) + (1. + pow(2.*M_PI,2))*cos(2.*M_PI*x(0));
+  return fx;
+}
